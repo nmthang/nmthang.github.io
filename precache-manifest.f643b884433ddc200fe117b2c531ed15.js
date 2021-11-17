20a8562516b68b4b94cd425b2587c46b47e24ff2
Thang Nguyen <thangnm.inbox@gmail.com>
2021-11-17 15:17:52 +0700

    Add wallet connect test

---
+++ precache-manifest.f643b884433ddc200fe117b2c531ed15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fce1e4aa5a6c9d3dfea",
+    "url": "/static/js/main.5fce1e4a.chunk.js"
+  },
+  {
+    "revision": "0114100051b36d6e2a4d",
+    "url": "/static/js/1.01141000.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "4d1e9ec5d2231f90e9ae978e28001ea3",
+    "url": "/index.html"
+  }
+];
